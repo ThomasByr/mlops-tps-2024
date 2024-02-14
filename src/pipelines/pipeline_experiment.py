c8912a90c10686c1bbe6a185a2c0c9e4309784aa
Maxime PETITPOISSON <87035896+m7415@users.noreply.github.com>
2024-02-14 20:53:07 +0100

    Feature/data extraction (#7)
    * Adding step prototypes and decommenting imports
    
    * clean proto
    
    * datasource extraction and dataset preparation
    
    * Formattage
    
    ---------
    
    Signed-off-by: Maxime PETITPOISSON <87035896+m7415@users.noreply.github.com>

--- src/pipelines/pipeline_experiment.py
+++ src/pipelines/pipeline_experiment.py
@@ -1,25 +1,28 @@
+import os
+
 from omegaconf import OmegaConf
 from zenml import pipeline
 
 from src.config.settings import EXTRACTED_DATASETS_PATH, MLFLOW_EXPERIMENT_PIPELINE_NAME
-from src.steps.data.data_extractor import dataset_extractor
+from src.models.model_dataset import Dataset
 from src.steps.data.datalake_initializers import (
     data_source_list_initializer,
     minio_client_initializer,
 )
+from src.steps.data.dataset_creators import dataset_creator
 from src.steps.data.dataset_preparators import (
-    dataset_creator,
-    dataset_to_yolo_converter,
+    dataset_preparator,
 )
-from src.steps.training.model_appraisers import model_appraiser
+from src.steps.data.datasources_extractor import datasources_extractor
 from src.steps.training.model_evaluators import model_evaluator
 from src.steps.training.model_trainers import (
     get_pre_trained_weights_path,
     model_trainer,
 )
+from src.steps.training.model_validation import model_validation
 
 
-@pipeline(name=MLFLOW_EXPERIMENT_PIPELINE_NAME)
+@pipeline(name=MLFLOW_EXPERIMENT_PIPELINE_NAME, enable_cache=False)
 def gitflow_experiment_pipeline(cfg: str) -> None:
     """
     Experiment a local training and evaluate if the model can be deployed.
@@ -28,24 +31,17 @@ def gitflow_experiment_pipeline(cfg: str) -> None:
         cfg: The Hydra configuration.
     """
     pipeline_config = OmegaConf.to_container(OmegaConf.create(cfg))
+    # pipeline_config = {'project': {'name': 'mlops-hardshat'}, 'model': {'epochs': 10}, 'evaluation': {'key': 'value'}, 'pipeline': {'name': 'experiment'}}
 
     bucket_client = minio_client_initializer()
     data_source_list = data_source_list_initializer()
 
-    # Prepare/create the dataset
-    # dataset = dataset_creator(
-    #     ...
-    # )
+    dataset = dataset_creator()
 
-    # Extract the dataset to a folder
-    # extraction_path = dataset_extractor(
-    #     ...
-    # )
+    destination_path = "./datasets/"
+    dataset_path = datasources_extractor(dataset, bucket_client, destination_path)
 
-    # If necessary, convert the dataset to a YOLO format
-    # dataset_to_yolo_converter(
-    #     ...
-    # )
+    dataset_preparator(dataset, dataset_path)
 
     # Train the model
     # trained_model_path = model_trainer(
